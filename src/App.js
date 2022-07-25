d0b6d32a7c9aa43df9f8133f6241ed81e2d3e755
Victoria Martynenko <105857981+v-victoria@users.noreply.github.com>
2022-07-25 22:02:18 +0100

    Added Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>My name is Victoria Martynenko</h1>
+        <h2>
+          I am a 3D Artist in the direction of Architectural Visualisation
+        </h2>
+        <h3>My Portfolio</h3>
+      </header>
+      <footer>
+        <div>
+          © 2022. This website was designed and coded by{" "}
+          <a href="/">Victoria Martynenko</a>, and it is{" "}
+          <a href="/">open-sourced</a>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
